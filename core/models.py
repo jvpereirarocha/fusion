e26e4ace3c352142dfe5e5973beb651f46486d55
Joao Victor <jvpereirarocha@gmail.com>
2020-03-15 00:01:31 -0300

    Alterando templates

--- core/models.py
+++ core/models.py
@@ -1,3 +1,68 @@
 from django.db import models
+import uuid
+from stdimage.models import StdImageField
 
-# Create your models here.
+
+def get_path_file(_instance, filename):
+    ext = filename.split('.')[-1]
+    filename = f"{uuid.uuid4()}.{ext}"
+    return filename
+
+
+class Base(models.Model):
+    criado = models.DateField(verbose_name='Criação', auto_now_add=True)
+    modificado = models.DateField(verbose_name='Modificado', auto_now=True)
+    ativo = models.BooleanField(verbose_name='Ativo', default=True)
+
+    class Meta:
+        abstract = True
+
+
+class Servico(Base):
+    ICONE_CHOICES = (
+        ('lni-cog', 'Engrenagem'),
+        ('lni-stats-up', 'Gráfico'),
+        ('lni-users', 'Usuários'),
+        ('lni-layers', 'Design'),
+        ('lni-mobile', 'Mobile'),
+        ('lni-rocket', 'Foguete'),
+    )
+    servico = models.CharField(verbose_name='Serviço', max_length=100)
+    descricao = models.TextField(verbose_name='Descrição', max_length=255)
+    icone = models.CharField(verbose_name='Ícone', max_length=30, choices=ICONE_CHOICES)
+
+    class Meta:
+        verbose_name = 'Serviço'
+        verbose_name_plural = 'Serviços'
+
+    def __str__(self):
+        return self.servico
+
+
+class Cargo(Base):
+    cargo = models.CharField(verbose_name='Cargo', max_length=100)
+
+    class Meta:
+        verbose_name = 'Cargo'
+        verbose_name_plural = 'Cargos'
+
+    def __str__(self):
+        return self.cargo
+
+
+class Funcionario(Base):
+    nome = models.CharField(verbose_name='Nome', max_length=100)
+    cargo = models.ForeignKey(Cargo, verbose_name='Cargo', on_delete=models.CASCADE)
+    bio = models.TextField(verbose_name='Bio', max_length=200)
+    imagem = StdImageField(verbose_name='Imagem', upload_to=get_path_file,\
+                           variations={'thumb': {'width': 480, 'height': 480, 'crop': True}})
+    facebook = models.CharField(verbose_name='Facebook', max_length=255, default='#')
+    twitter = models.CharField(verbose_name='Twitter', max_length=255, default='#')
+    instagram = models.CharField(verbose_name='Instagram', max_length=255, default='#')
+
+    class Meta:
+        verbose_name = 'Funcionário'
+        verbose_name_plural = 'Funcionários'
+
+    def __str__(self):
+        return self.nome
